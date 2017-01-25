20968d3af5eefba8686d8f0684b4ce5ea9b48b87
MadmanPL <michaltelega@interia.pl>
2017-01-25 23:32:14 +0100

    Simple use Picasso library

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.android.popularmovies;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView m_ivImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        m_ivImage = (ImageView)findViewById(R.id.iv_image);
+
+        Context context = this;
+        Picasso.with(context).load("http://i.imgur.com/DvpvklR.png").into(m_ivImage);
     }
 }
