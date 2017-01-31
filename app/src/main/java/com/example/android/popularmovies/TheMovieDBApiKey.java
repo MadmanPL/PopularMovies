3197ec5f01ba78cdd06e2f5a14ca1e6be3dad8b6
MadmanPL <michaltelega@interia.pl>
2017-01-31 21:18:30 +0100

    Class for provide API Key

---
+++ app/src/main/java/com/example/android/popularmovies/TheMovieDBApiKey.java
@@ -0,0 +1,12 @@
+package com.example.android.popularmovies;
+
+/**
+ * Created by Integra on 2017-01-26.
+ */
+
+public class TheMovieDBApiKey {
+    public static String GetApiKey()
+    {
+        return "";
+    }
+}
